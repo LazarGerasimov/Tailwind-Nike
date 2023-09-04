5a8c4d5710d74fd814c53e9599e06c821d1970a9
LazarGerasimov <lazgerasimov@gmail.com>
2023-09-04 22:26:16 +0300

    update Hero component: - update "Nike" span styles

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span>Nike</span>
+          <span className="text-coral-red inline-block mt-3">Nike</span>
           Shoes
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort and innovation for your active life.</p>
