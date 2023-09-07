b5bfcba00b1c3ea8f8d3878fca85127142cd719a
Laz <lazgerasimov@gmail.com>
2023-09-07 11:06:29 +0100

    add and import ShoeCard component scaffold

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
-import { statistics } from "../constants";
+import ShoeCard from "../components/ShoeCard";
+import { shoes, statistics } from "../constants";
 import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
 
@@ -30,8 +31,14 @@ const Hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} alt="shoe collection" width={610} height={500} className="object-contain relative z-10"/>
-        
+        <img src={bigShoe1} alt="shoe collection" width={610} height={500} className="object-contain relative z-10" />
+      </div>
+      <div>
+        {shoes.map((shoe) => (
+          <div key={shoe}>
+            <ShoeCard />
+          </div>
+        ))}
       </div>
     </section>
   );
