8e7929412ea2e4e09c6b62cd17e1ac55ef47988d
Laz <lazgerasimov@gmail.com>
2023-09-09 18:02:20 +0100

    update popular products heading styles

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -5,8 +5,8 @@ const PopularProducts = () => {
     <div>
       <section id="products" className="max-container max-sm:mt-12">
         <div className="flex flex-col justify-start gap-5">
-          <h2>Popular Products</h2>
-          <p>Experience top-notch quality and style with our selections. Discover a world of comfort, design and value</p>
+          <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+          <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our selections. Discover a world of comfort, design and value</p>
         </div>
       </section>
     </div>
