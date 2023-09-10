d4916cb263877a12b37b8517d7d0b491be3c820f
Laz <lazgerasimov@gmail.com>
2023-09-10 15:01:00 +0100

    update SuperQuality section: - add image and styles - complete section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,5 @@
 import Button from "../components/Button";
-
+import { shoe8 } from "../assets/images";
 
 const SuperQuality = () => {
   return (
@@ -19,6 +19,9 @@ const SuperQuality = () => {
         <div className="mt-11">
           <Button label="View details" />
         </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain"/>
       </div>
     </section>
   );
